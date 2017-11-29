846bd400bcd971d7dd560f747833c3b83d53ec07
Riki Montgomery <mostriki820@gmail.com>
2017-11-28 22:23:10 -0800

    finish lecture 26

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'error', to: 'pages#error'
+
+  get "blog", to: redirect("https://www.npr.org/")
+
+  resources :projects
+
+  root 'pages#home'
+  
+  get "*path", to: redirect("/error")
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
