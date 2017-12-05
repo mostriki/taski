b5d476684d03f70f4fd8480eb9aebbfe1be67ffb
Riki Montgomery <mostriki820@gmail.com>
2017-12-04 19:13:11 -0800

    validate presence of project input

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@ class Project < ApplicationRecord
   #callback default
   after_initialize :set_defaults
 
+  validates_presence_of :title, :description, :percent_complete
+
   scope :almost_completed, -> { where('percent_complete > 75.0') }
   scope :still_needs_some_work, -> { where('percent_complete < 75.0') }
 
