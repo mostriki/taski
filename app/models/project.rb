e52d703b1cf9dc6474b538ef044869a284249934
Riki Montgomery <mostriki820@gmail.com>
2017-12-04 19:10:06 -0800

    set defaults in project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ApplicationRecord
+  #callback default
+  after_initialize :set_defaults
+
+  scope :almost_completed, -> { where('percent_complete > 75.0') }
+  scope :still_needs_some_work, -> { where('percent_complete < 75.0') }
+
+  def set_defaults
+    self.percent_complete ||= 0.0
+  end
 end
