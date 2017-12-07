50231de02b338afeb48c387e92895b3e0bc690e0
Riki Montgomery <mostriki820@gmail.com>
2017-12-06 21:20:47 -0800

    added custom CRUD functionality and tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'tasks/show'
-
-  get 'tasks/new'
-
-  get 'tasks/edit'
-
   get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
   get 'error', to: 'pages#error'
 
   get "blog", to: redirect("https://www.npr.org/")
 
-  resources :projects
+  resources :projects do
+    resources :tasks, except: [:index], controller: 'projects/tasks'
+  end
 
   root 'pages#home'
-  
+
   get "*path", to: redirect("/error")
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
